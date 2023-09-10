34fe2e89895ef1803d6a1e9e8e86e58ebfeea38d
root <root@ip-172-31-21-57.ca-central-1.compute.internal>
2023-09-10 02:26:29 +0000

    Pipeline-Jenkins

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "!!!!!!!!!!!!!   We Are Building Git MAven Jenkins integration With Compile Package RUN Revview PIPELINE   !!!!!!!!!!!!!!!!!!!!" );
+    }
+}
